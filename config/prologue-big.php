eebb081916371f1fcb44ff6e95fe53eabae2dbca
Eric Percifield <epercifield@prologuetechnology.com>
2017-07-10 12:02:38 -0500

    Add a default dataset option.

--- config/prologue-big.php
+++ config/prologue-big.php
@@ -4,5 +4,6 @@ return [
     'big' => [
         'auth_file' => env('BIG_AUTH_FILE'),
         'project_id' => env('BIG_PROJECT_ID'),
+        'default_dataset' => env('BIG_DEFAULT_DATASET'),
     ],
 ];
